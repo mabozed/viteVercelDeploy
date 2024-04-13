eff2412ef97447947644a20298cacb095c5de361
mabozed <mabozed.com@gmail.com>
2024-04-13 22:25:06 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+    </Routes>
+  )
+}
+
+export default App
